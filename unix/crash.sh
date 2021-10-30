86f7030bdc2b97f954071bd98645404f10ca44f6
Tommy Zaft <76645845+Tom-stack3@users.noreply.github.com>
2021-10-30 17:55:42 +0300

    minor fix

--- unix/crash.sh
+++ unix/crash.sh
@@ -3,12 +3,14 @@
 echo "lol"
 gnome-terminal -- ./crash.sh
 source ./crash.sh
+sleep 5
 
 # Create and run from terminal:
 : '
 echo echo \"lol\" > crash.sh
 echo gnome-terminal -- ./crash.sh >> crash.sh
 echo source ./crash.sh >> crash.sh
+echo sleep 5 >> crash.sh
 chmod +x ./crash.sh
 ./crash.sh
 '
